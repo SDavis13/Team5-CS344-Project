2865310629d40ca17addfa9ff81a39c8129e91de
abuchanan11 <abuchanan11@winona.edu>
2015-11-18 14:11:28 -0600

    Changes to PhotoGallery

--- draft/photoGallery.php
+++ draft/photoGallery.php
@@ -65,7 +65,7 @@
 		Things and stuff here
 	</p>
 	<a id="donate" href="contact.html"><h3>Contact Us</h3></a>
-	<p id="copy">All content &copy; 2015, Min Lee.</p>
+	<p id="copy">All content &copy; 2015, CS344.</p>
 </footer> 
 </div> 
 </body>
