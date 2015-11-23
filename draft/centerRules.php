0b29f0decfc26ebef022fde421eca69a0f008e65
jbergeron12 <jbergeron12@jbergeron12m5.wireless.winona.edu>
2015-11-23 10:04:51 -0600

    For the about section (History and Center Rules)

---
+++ draft/centerRules.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php $title = "Center Rules" ?>
+	<?php include 'head.php'; ?>
+</head>
+<body onload="daily()">
+<div id="wrapper">
+<?php include 'header.php'; ?>
+<div id="main">
+	<!-- column 1 !-->
+	<div id="left"> 
+		<div class="border"> 
+			<span class="title">Rule 1-Respect other kids:</span> 
+			<p>
+				No hitting/pushing<br>
+				No name-calling<br>
+				No swearing<br>
+			</p>
+		</div>
+		<div class="border"> 
+			<span class="title">Rule 3-Get your homework done before you play:</span> 
+			<p>
+				The first hour is homework time. Students do their homework
+				and spend time reading with an adult
+			</p>
+		</div>
+		<div class="border"> 
+			<span class="title">Discipline Procedure</span> 
+			<p>
+				1.) Warning:<br>
+				The violation is explained and the warning is noted<br>
+				2.) Time-Out:<br>
+				A 15 minute time-out is given after the 2nd violation.
+				For the older kids a time-out is being taken out of the building.
+				Children in kindergarten or first grade may be sent to a time-out chair
+				by the front desk.<br>
+				3.) Dismissal:<br>
+				The child is sent home for the night. We want to keep kids in the center
+				when possible, so dismissal is used for repeated violation or more serious behavior.	
+			</p>
+		</div>
+	</div>
+	<!-- column 1 !-->
+	<div id="left">
+		<div class="border"> 
+				<span class="title">Rule 2-Respect the adults:</span> 
+			<p>
+				Listen to them<br>
+				Do what they say<br>
+			</p>
+		</div>
+		<div class="border"> 
+				<span class="title">Rule 4-Be helpful:</span> 
+			<p>
+				Help each other<br>
+				Help clean up<br>
+				Put away games that you check out<br>
+			</p>
+		</div>
+	</div>
+	<div id="centering"></div>
+</div>
+<?php include 'footer.php' ?>
+</div> 
+</body>
+</html>
---
+++ draft/history.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php $title = "History" ?>
+	<?php include 'head.php'; ?>
+</head>
+<body onload="daily()">
+<div id="wrapper">
+<?php include 'header.php'; ?>
+<div id="main">
+	<h1>History</h1>
+	<p>	
+			Kids First is an after school program for the Maplewood 
+		neighborhood in Winona Minnesota. Kids First was Founded 8 
+		years ago by Carol Marchant. There are about 80 families and 
+		200 kids living in Maplewood. Around 50 kids come in a night for 
+		homework help and fun. Each Semester, over 400 WSU students 
+		provide tutoring and recreational opportunities to the children
+		living in Maplewood. 
+	</p>
+	</div>
+	<div id="centering"></div>
+</div>
+<?php include 'footer.php' ?>
+</div> 
+</body>
+</html>
