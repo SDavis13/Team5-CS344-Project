b2f98f737e60b7b4c7da94e6e48078012019daf8
ActualSalt <minlee0702@gmail.com>
2015-11-23 21:07:33 -0600

    Nav text white
    Added "whitetxt" class to change color to white.

--- draft/header.php
+++ draft/header.php
@@ -8,18 +8,18 @@
 			<p><a href="home.php">Home</a></p>	
 		</li>
 		<li id="extend2" class="orange">
-			<p>About Us</p>
+			<p class="whitetxt">About Us</p>
 			<p class="subtext"><a href="history.php">History</a></p>
 			<p class="subtext"><a href="centerRules.php">Center Rules</a></p>
 		</li>
 		<li id="extend2" class="orange">
-			<p>Resources</p>
+			<p class="whitetxt">Resources</p>
 			<p class="subtext"><a href="kids.php">Kids</a></p>
 			<p class="subtext"><a href="parents.php">Parents</a></p>
 			<p class="subtext"><a href="students.php">WSU Students</a></p>
 		</li>
 		<li id="extend1" class="orange">
-			<p>Get Involved</p>
+			<p class="whitetxt" >Get Involved</p>
 			<p class="subtext"><a href="volunteer.php">Volunteering</a></p>
 			<p class="subtext"><a href="supportus.php">Support Us</a></p>
 		</li>
