8a2f56d18e43339528129617df7ec78887abd144
jbergeron12 <jbergeron12@jbergeron12m5.wireless.winona.edu>
2015-11-30 17:09:33 -0600

    Kids
    Fun websites

--- draft/kids.php
+++ draft/kids.php
@@ -7,6 +7,42 @@
 <div id="wrapper">
 <?php include 'header.php'; ?>
 <div id="main">
+	<div id="left"> 
+		<div class="border"> 
+			<span class="title">Math:</span> 
+			<p>
+				<a href="http://www.online-calculator.com/full-screen-calculator/">Online Calculator!</a><br>
+				<a href="http://pbskids.org/games/math/">Math Games!</a><br>
+			</p>
+		</div>
+	</div>
+	<div id="left"> 
+		<div class="border"> 
+			<span class="title">Science:</span> 
+			<p>
+				<a href="http://pbskids.org/zoom/activities/sci/">Fun Experiments!</a><br>
+				<a href="http://pbskids.org/games/science/">Science Games!</a><br>
+			</p>
+		</div>
+	</div>
+	<div id="left"> 
+		<div class="border"> 
+			<span class="title">Reading:</span> 
+			<p>
+				<a href="http://www.storylineonline.net">Online Reading of Books!</a><br>
+				<a href="http://pbskids.org/games/reading/">Readings Games!</a><br>
+			</p>
+		</div>
+	</div>
+	<div id="left"> 
+		<div class="border"> 
+			<span class="title">Social Studies:</span> 
+			<p>
+				<a href="http://pbskids.org/games/social-studies/">Social Studies Games!</a><br>
+			</p>
+		</div>
+	</div>
+	<div id="centering"></div>
 </div>
 <?php include 'footer.php' ?>
 </div> 
