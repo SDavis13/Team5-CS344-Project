0e526c387ab3349c833757d822bf0558cdba6957
jbergeron12 <jbergeron12@jbergeron12m5.wireless.winona.edu>
2015-11-30 17:34:07 -0600

    Parents
    parents content

--- draft/parents.php
+++ draft/parents.php
@@ -7,6 +7,14 @@
 <div id="wrapper">
 <?php include 'header.php'; ?>
 <div id="main">
+<h1>Parents:</h1>
+	<p>
+		-->In order for your kids to attend Kids First, you must live in Maplewood.<br><br>
+		-->We open at 2:30PM and close at 6:30PM<br><br>
+		-->2:30-3:30--Is designated for homework<br><br>
+		-->3:30-6:30--Is free time! Snacks, games, crafts, and scheduled activities.<br><br>
+		-->Child safety is a top Priority! No one can volunteer if they have a background of <em>Harming Children</em> or <em>Charges with weapons</em>!
+	</p>
 </div>
 <?php include 'footer.php' ?>
 </div> 
