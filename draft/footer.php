1b381735ca79ed7346f8c5fff6f350472f381e87
jbergeron12 <jbergeron12@jbergeron12m5.wireless.winona.edu>
2015-12-02 09:51:35 -0600

    footer changes

--- draft/footer.php
+++ draft/footer.php
@@ -1,8 +1,9 @@
 <!-- footer !-->
 <footer>
-	<p id="about">
-		Things and stuff here
-	</p>
+	<p>Partners:</p>
+	<a id="partners" href="http://www.winona.edu"><img  src="images/WSUfooter.png" alt="WSU" style="width:60px;height:90px;"></a>
+	<a id="partners" href="http://www.winonahra.org"><img  src="images/WHRA.png" alt="WHRA" style="width:60px;height:90px;"></a>
 	<a id="donate" href="contact.php"><h3>Contact Us</h3></a>
 	<p id="copy">All content &copy; 2015, CS344.</p>
+	
 </footer> 
