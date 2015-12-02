6c79506a7c0fc4855151725e64524a17043e7945
ActualSalt <minlee0702@gmail.com>
2015-12-02 12:43:20 -0600

    Style change in Footer
    Changed Partners: to white and moved the images to the right a bit.
    
    Moved the css footer elements under footer comment for organization.

--- draft/footer.php
+++ draft/footer.php
@@ -1,8 +1,8 @@
 <!-- footer !-->
 <footer>
-	<p>Partners:</p>
+	<p class="whitetxt">Partners:</p>
 	<a id="partners" href="http://www.winona.edu"><img  src="images/WSUfooter.png" alt="WSU" style="width:60px;height:90px;"></a>
-	<a id="partners" href="http://www.winonahra.org"><img  src="images/WHRA.png" alt="WHRA" style="width:60px;height:90px;"></a>
+	<a id="partners2" href="http://www.winonahra.org"><img  src="images/WHRA.png" alt="WHRA" style="width:60px;height:90px;"></a>
 	<a id="donate" href="contact.php"><h3>Contact Us</h3></a>
 	<p id="copy">All content &copy; 2015, CS344.</p>
 	
