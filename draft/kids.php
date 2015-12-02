0ca376f11cd1f2cc7e60184874726ec24a95d792
ActualSalt <minlee0702@gmail.com>
2015-12-02 12:35:17 -0600

    Kid page color correction
    The new nav system won't work with the regualr a href.
    For the future text links, always color that in.

--- draft/kids.php
+++ draft/kids.php
@@ -13,8 +13,8 @@
 			<span class="title">Math:</span> 
 			<img id="photo" src="images/math.jpeg" alt="Math Logo" style="width:100px;height:100px;">
 			<p>
-				<a href="http://www.online-calculator.com/full-screen-calculator/">Online Calculator!</a><br>
-				<a href="http://pbskids.org/games/math/">Math Games!</a><br>
+				<a class="kids" href="http://www.online-calculator.com/full-screen-calculator/">Online Calculator!</a><br>
+				<a class="kids" href="http://pbskids.org/games/math/">Math Games!</a><br>
 			</p>
 		</div>
 	</div>
@@ -23,8 +23,8 @@
 			<span class="title">Science:</span> 
 			<img id="photo" src="images/science.jpeg" alt="Science Materials" style="width:100px;height:100px;">
 			<p>
-				<a href="http://pbskids.org/zoom/activities/sci/">Fun Experiments!</a><br>
-				<a href="http://pbskids.org/games/science/">Science Games!</a><br>
+				<a class="kids" href="http://pbskids.org/zoom/activities/sci/">Fun Experiments!</a><br>
+				<a class="kids" href="http://pbskids.org/games/science/">Science Games!</a><br>
 			</p>
 		</div>
 	</div>
@@ -33,8 +33,8 @@
 			<span class="title">Reading:</span> 
 			<img id="photo" src="images/reading.jpeg" alt="Books" style="width:100px;height:100px;">
 			<p>
-				<a href="http://www.storylineonline.net">Online Reading of Books!</a><br>
-				<a href="http://pbskids.org/games/reading/">Readings Games!</a><br>
+				<a class="kids" href="http://www.storylineonline.net">Online Reading of Books!</a><br>
+				<a class="kids" href="http://pbskids.org/games/reading/">Readings Games!</a><br>
 			</p>
 		</div>
 	</div>
@@ -43,7 +43,7 @@
 			<span class="title">Social Studies:</span> 
 			<img id="photo" src="images/socialStudies.jpeg" alt="Social Studies" style="width:100px;height:100px;">
 			<p>
-				<a href="http://pbskids.org/games/social-studies/">Social Studies Games!</a><br>
+				<a class="kids" href="http://pbskids.org/games/social-studies/">Social Studies Games!</a><br>
 			</p>
 		</div>
 	</div>
