d3dcf65e81439b8749a8d9aed22511e058fdd8b7
Spencer Davis <SDavis13@winona.edu>
2015-12-01 19:27:49 -0600

    history change

--- draft/history.php
+++ draft/history.php
@@ -10,16 +10,9 @@
 <main>
 	<article>
 		<h1>History</h1>
-		<p>	
-				Kids First is an after school program for the Maplewood 
-			neighborhood in Winona Minnesota. Kids First was Founded 8 
-			years ago by Carol Marchant. There are about 80 families and 
-			200 kids living in Maplewood. Around 50 kids come in a night for 
-			homework help and fun. Each Semester, over 400 WSU students 
-			provide tutoring and recreational opportunities to the children
-			living in Maplewood. 
+		<p>
+			<?= file_get_contents("history.txt"); ?>
 		</p>
-		</div>
 	</article>
 	<div id="centering"></div>
 </main>
