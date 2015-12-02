3341d60260403bb08d987103a9dd43005c30e3c7
jbergeron12 <jbergeron12@jbergeron12m5.wireless.winona.edu>
2015-12-01 19:53:58 -0600

    Photos
    Photos in the kids section

--- draft/kids.php
+++ draft/kids.php
@@ -6,11 +6,12 @@
 </head>
 <div id="wrapper">
 <?php include 'header.php'; ?>
-<<<<<<< HEAD
-<div id="main">
+
+<main>
 	<div id="left"> 
 		<div class="border"> 
 			<span class="title">Math:</span> 
+			<img id="photo" src="images/math.jpeg" alt="Math Logo" style="width:100px;height:100px;">
 			<p>
 				<a href="http://www.online-calculator.com/full-screen-calculator/">Online Calculator!</a><br>
 				<a href="http://pbskids.org/games/math/">Math Games!</a><br>
@@ -20,6 +21,7 @@
 	<div id="left"> 
 		<div class="border"> 
 			<span class="title">Science:</span> 
+			<img id="photo" src="images/science.jpeg" alt="Science Materials" style="width:100px;height:100px;">
 			<p>
 				<a href="http://pbskids.org/zoom/activities/sci/">Fun Experiments!</a><br>
 				<a href="http://pbskids.org/games/science/">Science Games!</a><br>
@@ -29,6 +31,7 @@
 	<div id="left"> 
 		<div class="border"> 
 			<span class="title">Reading:</span> 
+			<img id="photo" src="images/reading.jpeg" alt="Books" style="width:100px;height:100px;">
 			<p>
 				<a href="http://www.storylineonline.net">Online Reading of Books!</a><br>
 				<a href="http://pbskids.org/games/reading/">Readings Games!</a><br>
@@ -38,17 +41,15 @@
 	<div id="left"> 
 		<div class="border"> 
 			<span class="title">Social Studies:</span> 
+			<img id="photo" src="images/socialStudies.jpeg" alt="Social Studies" style="width:100px;height:100px;">
 			<p>
 				<a href="http://pbskids.org/games/social-studies/">Social Studies Games!</a><br>
 			</p>
 		</div>
 	</div>
 	<div id="centering"></div>
-</div>
-=======
-<main>
 </main>
->>>>>>> origin/master
+
 <?php include 'footer.php' ?>
 </div> 
 </body>
