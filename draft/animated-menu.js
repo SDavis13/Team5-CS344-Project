a612ba3c75eeb034dabba90524de9267e1c87bd3
Spencer Davis <SDavis13@winona.edu>
2015-12-01 19:44:06 -0600

    fixed dropdown for firefox

--- draft/animated-menu.js
+++ draft/animated-menu.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
 	$("li").mouseover(function(){
 		$(this).stop().animate({height:'50px'},{queue:false, duration:600, easing: 'easeOutBounce'})
 	});
-	$(extend2).mouseover(function(){
+	$(".extend2").mouseover(function(){
 		$(this).stop().animate({height:'175px'},{queue:false, duration:600, easing: 'easeOutBounce'})
 	});
-	$(extend3).mouseover(function(){
+	$(".extend3").mouseover(function(){
 		$(this).stop().animate({height:'230px'},{queue:false, duration:600, easing: 'easeOutBounce'})
 	});
 
