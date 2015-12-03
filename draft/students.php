4e5761f024ea17bc0cfe9ec576de5e1a832384a8
jbergeron12 <jbergeron12@jbergeron12m5.local>
2015-12-03 16:54:45 -0600

    rules

--- draft/students.php
+++ draft/students.php
@@ -12,7 +12,15 @@
 			<span class="title">How to Volunteer:</span> 
 			<p>
 				1.) Visit your D2L page and click on Self Registration. Next scroll down and click Kids First. Then click register.<br>
-				2.) 
+				2.) You will need 2 background check forms. One you will find under the content section of the Kids First page. The second one
+					will be from the state that you were born in. You will also need to bring a copy of your Drivers Licence.<br>
+				3.) You will need to complete the online quiz. Both the quiz form and the tutorials are in the materials/content section.
+					Review the tutorials, and bring the completed quiz with you.<br>
+				4.) Kids First Picture ID: This can be obtained from the Campus Card Office by the Registrar Hub. Pay $3 at the Cashier 
+					Office, and then go to the Campus Card Office (Maxwell 227) and tell them you need a photo ID for the Kids First program. 
+					Bring the paid slip with you to the Campus Card Office.<br>
+				5.) <bold>Note: These are child safety issues. You cannot volunteer until all are complete.<br>
+					
 			</p>
 		</div>
 	
