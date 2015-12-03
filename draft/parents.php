5facbf3e0b2554fa97d2d7e58c0905521b917242
Alex Buchanan <abuchanan11@winona.edu>
2015-12-03 23:00:05 +0000

    parents page styling

--- draft/parents.php
+++ draft/parents.php
@@ -8,14 +8,20 @@
 <?php include 'header.php'; ?>
 
 <main>
-<h1>Parents:</h1>
-	<p>
-		-->In order for your kids to attend Kids First, you must live in Maplewood.<br><br>
-		-->We open at 2:30PM and close at 6:30PM<br><br>
-		-->2:30-3:30--Is designated for homework<br><br>
-		-->3:30-6:30--Is free time! Snacks, games, crafts, and scheduled activities.<br><br>
-		-->Child safety is a top Priority! No one can volunteer if they have a background of <em>Harming Children</em> or <em>Charges with weapons</em>!
-	</p>
+	<div id="full">
+			<div class="border"> 
+				<span class="title">Parents</span>
+				<p>
+					<h3>Here is some useful information for parents interested in Kids First</h3>
+					In order for your kids to attend Kids First, you must live in Maplewood.<br>
+					We open at 2:30PM and close at 6:30PM<br>
+					2:30-3:30--Is designated for homework<br>
+					3:30-6:30--Is free time! Snacks, games, crafts, and scheduled activities.<br>
+					Child safety is a top Priority! No one can volunteer if they have a background of <em>Harming Children</em> or <em>Charges with weapons</em>!
+				</p>
+			</div>
+		</div>
+		<div id="centering"></div>
 </main>
 
 <?php include 'footer.php' ?>
